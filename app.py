00a99a05a2cad79d2b55e61d8b96930acb50fbdf
Hatori Hunzo <hunzo@192.168.1.9>
2019-05-09 00:01:25 +0700

    :whale:

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, jsonify
+# from flask_wtf import FlaskForm, RecaptchaField
+from flask_wtf import FlaskForm
+from wtforms.fields.html5 import DateField
+from wtforms import StringField
+from wtforms.validators import InputRequired
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'test'
+# app.config['RECAPTCHA_PUBLIC_KEY'] = 'XX'
+# app.config['RECAPTCHA_PRIVATE_KEY'] = 'XX'
+Bootstrap(app)
+
+
+class dateForm(FlaskForm):
+    DateForm = DateField('Expired Date', format='%Y-%m-%d',
+                         validators=[InputRequired(message='Expired date has Required')])
+    Username = StringField('Username', validators=[
+                           InputRequired('A Username has Required')])
+    # recaptcha = RecaptchaField()
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def index():
+    form = dateForm()
+    if form.validate_on_submit():
+        ret = {
+            'username': form.Username.data,
+            'unixTime': form.DateForm.data.strftime('%s'),
+            'ldapTime': (int(form.DateForm.data.strftime('%s')) + 11644473600) * 10000000
+        }
+        return jsonify({'data': ret})
+    return render_template('form.html', form=form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
